8c6117bd8de5d7b172a55895b1a00211143d55b0
ponyPharmacist <evilglen@gmail.com>
2012-03-16 12:13:00 +0000

    massively reworked js code

--- snapboard/media/js/thread.js
+++ snapboard/media/js/thread.js
@@ -1,104 +1,110 @@
+function $(selector) {
+  return bonzo(qwery(selector));
+}
 
 // AJAX-loaded forms
-function t_e(id) {
-  toggle('pt'+id, 'block');
-  return exttoggle_f('replies', 'block', 'action=geteditform&oid='+id); }
-function t_r(id) {
-  return exttoggle_f('replies', 'block', 'action=getreplyform&oid='+id); }
+function t_e(id) { return load_form('action=geteditform&oid='+id); }
+function t_r(id) { return load_form('action=getreplyform&oid='+id); }
 
-// thread level functions
-function set_csticky(id) { return exttoggle_v('replies', 'block',
-  'action=csticky&oclass=thread&oid='+id);}
-function set_gsticky(id) { return exttoggle_v('replies', 'block',
-  'action=gsticky&oclass=thread&oid='+id);}
-function set_close(id) { return exttoggle_v('replies', 'block',
-  'action=close&oclass=thread&oid='+id);}
+function load_form(action) {
+  var container = $('#replies');
+  container.html("<p>" + "Processing..." + "</p>");
+  var request = reqwest({
+    url: SNAPBOARD_URLS.rpc_action, 
+    type: 'json', 
+    method: 'post', 
+    data: action, 
+    error: function (err) { container.html('<p><b>Error:</b>'+ err.responseText +'</p>') },
+    success: function (resp) { container.html(resp.html) }
+  }); 
+  return false; 
+}
 
-// post level function
-function s_w(id) {
-  return exttoggle_v('replies', 'block',
-    'action=watch&oclass=post&oid='+id); }
-function s_c(id) {
-  return exttoggle_v('replies', 'block',
-    'action=censor&oclass=post&oid='+id); }
-function s_a(id) {
-  return exttoggle_v('replies', 'block',
-    'action=abuse&oclass=post&oid='+id); }
- 
-function toggle(id, type) {
-  var e = document.getElementById(id);
-  if(e.style.display == 'none')
-   e.style.display = type;
-  else
-   e.style.display = 'none'; }
+// Thread level functions
+function set_csticky(id) { return load_message('action=csticky&oclass=thread&oid='+id);}
+function set_gsticky(id) { return load_message('action=gsticky&oclass=thread&oid='+id);}
+function set_close(id) { return load_message('action=close&oclass=thread&oid='+id);}
 
-function exttoggle(e, type, action, successProcess) {
-  var handleSuccess = function(o) {
-    if(o.responseText !== undefined) {
-      res = JSON.parse(o.responseText);
-      successProcess(res);}};
-    // XXX: Do something if no response text?
-  var handleFailure = function(o) {
-	e.style.display = 'block';
-    e.innerHTML = "<p>" + "ERROR." + "</p>";
-    if(o.responseText !== undefined) {
-      for (var n in o) {
-        if (o.hasOwnProperty(n)) {
-          e.innerHTML += o[n]+"<br/>"; }}}};
-  var callback = {
-    success:handleSuccess,
-    failure:handleFailure,
-    argument:[]};
-  e.innerHTML = "<p>" + "Processing..." + "</p>";
-  var request = YAHOO.util.Connect.asyncRequest(
-    'POST',
-    SNAPBOARD_URLS.rpc_action, callback, action); }
+// Post level function
+function s_w(id) { return load_message('action=watch&oclass=post&oid='+id); }
+function s_c(id) { return load_message('action=censor&oclass=post&oid='+id); }
+function s_a(id) { return load_message('action=abuse&oclass=post&oid='+id); }
 
-function exttoggle_f(id, type, action) {
-    /* Toggle visibility of a block (e.g. form), retrieving its code
-      from server if necessary. */
-    var e = document.getElementById(id);
-    if(e.style.display == 'none') {
-		e.style.display = 'block';
-        var successProcess = function(res) {
-            e.innerHTML = res.html; };
-        exttoggle(e, type, action, successProcess); }
-    else {
-        e.style.display = 'none'; }
-    // Return false over all the chain is needed for links not to go over their href when clicked.
-    return false; }
+function load_message(action) {
+  var container = $('#replies');
+  container.html("<p>" + "Processing..." + "</p>");
+  
+  var request = reqwest({
+    url: SNAPBOARD_URLS.rpc_action, 
+    type: 'json', 
+    method: 'post', 
+    data: action, 
+    error: function (err) { container.html('<p><b>Error:</b>'+ err.responseText +'</p>') },
+    success: function (resp) { container.html('<p><b>Success:</b>' + resp.msg + ' <a href="">' +resp.link + '</a>?</p>') }
+  });
+  return false; 
+}
 
-function exttoggle_v(id, type, action) {
-    // var t = event.target;  // This is meant to be called onclick, yes.
-    var e = document.getElementById(id);
-    // t.innerHTML = 'just testing';
-    var successProcess = function(res) {
-        // t.innerHTML = res['link'];
-        e.innerHTML = '<p class="rm">' + res['msg'] + '</p>';};
-    exttoggle(e, type, action, successProcess);
-    return false; }
-    
+function preview(form_id, parent) {
+  var text = $('#'+form_id).val();
+  var parentpost = $('.nf[id='+parent+']');
+  var request = reqwest({
+    url: SNAPBOARD_URLS.rpc_preview, 
+    type: 'json', 
+    method: 'post', 
+    data: text, 
+    error: function (err) { parentpost.append(err.responseText) },
+    success: function (resp) { parentpost.append(
+	'<div class="fp l"><div class="h"><div><div><div class="hm"> <b>Username</b>: post preview </div></div></div></div><div><div class="v" style="margin-left:6px"><div id="preview" class="t"><div>' + resp.preview + '</div></div></div></div></div>'
+	) }
+  });
+return false; 
+}
 
-function preview(form_id) {
-    urlq = SNAPBOARD_URLS.rpc_preview;
-    form = document.getElementById(form_id);
-    text = form.post.value;
-    div_preview = document.getElementById('snap_preview_addpost');
-    var handleSuccess = function(o) {
-     if(o.responseText !== undefined) {
-      res = eval('(' + o.responseText + ')');
-      div_preview.innerHTML = res['preview'];
-      div_preview.parentNode.style.display = 'block';}};
-    var handleFailure = function(o) {
-     var errordiv = document.getElementById("thread_rpc_feedback");
-     if(o.responseText !== undefined) {
-      div_preview.innerHTML = 'There was an error previewing your post.';
-      div_preview.parentNode.style.display = 'block';}};
-    var callback = {success:handleSuccess, failure:handleFailure, argument: []};
-    YAHOO.util.Connect.setDefaultPostHeader(false);
-    YAHOO.util.Connect.initHeader('Content-Type', 'text/plain', true);
-    var request = YAHOO.util.Connect.asyncRequest('POST', urlq, callback, text);}
 
+/*Hide|unhide thread | written in pure JS */
+function xpath(path, root, type) {
+  if(type)
+    return document.evaluate(path, root || document, null, 8, null).singleNodeValue;
+  else {
+    var r = document.evaluate(path, root || document, null, 6, null), a = [];
+    for(var i=0;i<r.snapshotLength;i++) 
+      a.push(r.snapshotItem(i));
+    return a;
+  }
+}
+function each(list, func) { 
+  for(var i=0;i<list.length;i++)
+    func(list[i],i);
+}
+function switchid(id) {
+  var elink = document.getElementById(id+'_a');
+  var ediv = document.getElementById(id+'_d');
+  var ehead = document.getElementById('inh'+id);
+	
+  if(ediv.style.display == 'none') {
+    ediv.style.display = 'block';
+    elink.innerHTML = '-';  
+    ehead.style.borderBottomWidth = '0px';
+  } else {
+    ediv.style.display = 'none';
+    elink.innerHTML = '+';
+    ehead.style.borderBottomWidth = '1px';
+  }
+}
+function build() {
+  var xp = xpath('.//a[contains(@class,"hide")]');
+  each(xp, function(e){
+    e.addEventListener('click', function(){
+      switchid(this.id.split('_')[0]);
+    }, false);
+  })
+}
+window.onload = function() {
+  build();
+}
+
+// Rudimentary and partially nonfunctional code. ToDo: Upgrade to new or delete.
 function rev(orig_id, show_id) {
  urlq = SNAPBOARD_URLS.rpc_postrev + '?orig=' + orig_id + '&show=' + show_id;
  div_text = document.getElementById('pt' + orig_id);
@@ -203,45 +209,3 @@ function insert_link(url, name) {
  return do_insert('[url=' + url + ']' + name + '[/url]')}
 
 function find_textarea (el) { return true; }// el.tagName.toLowerCase() == 'textarea'; }
-
-/*Hide|unhide thread */
-function xpath(path, root, type) {
-    if(type)
-        return document.evaluate(path, root || document, null, 8, null).singleNodeValue;
-    else {
-        var r = document.evaluate(path, root || document, null, 6, null), a = [];
-        for(var i=0;i<r.snapshotLength;i++) 
-            a.push(r.snapshotItem(i));
-        return a;
-    }
-}
-function each(list, func) { 
-        for(var i=0;i<list.length;i++)
-            func(list[i],i);
-}
-function switchid(id) {
-    var elink = document.getElementById(id+'_a');
-    var ediv = document.getElementById(id+'_d');
-	var ehead = document.getElementById('inh'+id);
-	
-    if(ediv.style.display == 'none') {
-        ediv.style.display = 'block';
-        elink.innerHTML = '-';  
-		ehead.style.borderBottomWidth = '0px';
-    } else {
-        ediv.style.display = 'none';
-        elink.innerHTML = '+';
-		ehead.style.borderBottomWidth = '1px';
-    }
-}
-function build() {
-    var xp = xpath('.//a[contains(@class,"hide")]');
-    each(xp, function(e){
-        e.addEventListener('click', function(){
-            switchid(this.id.split('_')[0]);
-        }, false);
-    })
-}
-window.onload = function() {
-    build();
-}
